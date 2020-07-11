74f47a151d68034161127e024b41bd0ec0905b05
Rem <hug211mire@gmail.com>
2020-07-11 13:05:55 +0200

    add cli crawl command

--- main.go
+++ main.go
@@ -20,46 +20,52 @@ func main() {
 		Name:  "ReconArchy",
 		Usage: "Crawl 1000 employees of a choosen company and build their organizational chart",
 
-		Flags: []cli.Flag{
-			&cli.StringFlag{
-				Name:        "threads",
-				Value:       "1",
-				Aliases:     []string{"t"},
-				Usage:       "Adjust number of crawling worker",
-				Destination: &threads,
-			},
-			&cli.StringFlag{
-				Name:        "company",
-				Aliases:     []string{"c"},
-				Usage:       "Name of the target company",
-				Destination: &company,
-			},
-		},
-		Action: func(c *cli.Context) error {
+		Commands: []*cli.Command{
+			{
+				Name:  "crawl",
+				Usage: "crawl employees specific to a company",
 
-			// ARGUMENT HANDLING (in one Action - for now there is no commands or subcommands)
-			if company == "" {
-				fmt.Println("Please choose a company to target")
-				os.Exit(0)
-			}
-			threadsInt, errConv := strconv.Atoi(threads)
-			if errConv != nil {
-				panic(errConv)
-			}
-			if threadsInt > 4 {
-				fmt.Printf("\nBy setting THREADS to %d, you will spawn %d WebDriver on your machine.", threadsInt, threadsInt)
-				fmt.Printf("\nI think she can not handle it.\nSetup THREAD to 4 at max the next time.")
-				threadsInt = 4
-				fmt.Printf("\nAuto - Setting THREADS to 4\n")
-			}
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:        "threads",
+						Value:       "1",
+						Aliases:     []string{"t"},
+						Usage:       "Adjust number of crawling worker",
+						Destination: &threads,
+					},
+					&cli.StringFlag{
+						Name:        "company",
+						Aliases:     []string{"c"},
+						Usage:       "Name of the target company",
+						Destination: &company,
+					},
+				}, // Crawl Command Flags
 
-			// MAIN - START PROGRAM
+				Action: func(c *cli.Context) error {
+					// ARGUMENT HANDLING (in one Action - for now there is only one commands and no subcommands)
+					if company == "" {
+						fmt.Println("Please choose a company to target")
+						os.Exit(0)
+					}
+					threadsInt, errConv := strconv.Atoi(threads)
+					if errConv != nil {
+						panic(errConv)
+					}
+					if threadsInt > 4 {
+						fmt.Printf("\nBy setting THREADS to %d, you will spawn %d WebDriver on your machine.", threadsInt, threadsInt)
+						fmt.Printf("\nI think she can not handle it.\nSetup THREAD to 4 at max the next time.")
+						threadsInt = 4
+						fmt.Printf("\nAuto - Setting THREADS to 4\n")
+					}
 
-			selenium.Start(company, threadsInt)
+					// MAIN - START PROGRAM
+					selenium.Start(company, threadsInt)
 
-			return nil
-		},
-	}
+					return nil
+				}, // Action
+			}, // Command command
+		}, // Command main
+	} // App
 
 	// Run apps cli
 	err := app.Run(os.Args)
